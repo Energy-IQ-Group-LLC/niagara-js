58e68239be4103030b6e8601b3c804ec14de5a76
Adam Zimmerman <64041152+adamzman@users.noreply.github.com>
2024-12-01 19:04:15 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/main.js'),
+      entry: resolve(__dirname, 'index.js'),
       name: 'niagara-js',
       fileName: (format) => `niagara-js.${format}.js`,
     },
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './lib'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
