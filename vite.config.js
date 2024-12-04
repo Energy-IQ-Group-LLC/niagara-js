e636c7ee2b8e2121fcb19c7967559e0346bffe9d
Adam Zimmerman <64041152+adamzman@users.noreply.github.com>
2024-12-04 08:59:42 -0500

    exclude building of dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+import { builtinModules } from 'module';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { dependencies } from '@root/package.json';
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'index.js'),
       name: 'niagara-js',
       fileName: (format) => `niagara-js.${format}.js`,
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: [
+        ...builtinModules, // Exclude Node.js built-ins
+        ...Object.keys(dependencies || {}), // Exclude package dependencies
+      ],
     },
   },
   resolve: {
