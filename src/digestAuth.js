02e9170539864eb31310f37ba0a1fc6b427be976
Adam Zimmerman <64041152+adamzman@users.noreply.github.com>
2024-12-04 09:01:57 -0500

    properly close jsdom window for digestLogin

--- src/digestAuth.js
+++ src/digestAuth.js
@@ -1,6 +1,6 @@
+import { stripTrailingSlash } from '@/helpers.js';
 import { JSDOM } from 'jsdom';
 import vm from 'vm';
-import { stripTrailingSlash } from './helpers.js';
 
 /**
  * @param {string} siteUrl
@@ -9,40 +9,42 @@ import { stripTrailingSlash } from './helpers.js';
  * @param {string} auth.password
  */
 export async function digestAuthLogin(siteUrl, { username, password }) {
-  siteUrl = stripTrailingSlash(siteUrl);
+  const siteUrlFormatted = stripTrailingSlash(siteUrl);
 
-  const scriptUrl = `${siteUrl}/login/core/auth.min.js`;
+  const scriptUrl = `${siteUrlFormatted}/login/core/auth.min.js`;
   const scriptResponse = await fetch(scriptUrl);
   const scriptContent = await scriptResponse.text();
 
-  const dom = new JSDOM('', { url: siteUrl });
+  const dom = new JSDOM('', { url: siteUrlFormatted });
   const { window } = dom;
 
-  vm.createContext(window); // Set up the context
-  vm.runInContext(scriptContent, window); // Run the auth.min.js script
+  try {
+    vm.createContext(window); // Set up the context
+    vm.runInContext(scriptContent, window); // Run the auth.min.js script
 
-  // This gets the JSESSIONID cookie to be set on the jsdom window
-  await new Promise((resolve, reject) => {
-    window.ScramShaClient.authenticate(`${siteUrl}/`, username, password, window.sjcl.hash.sha256, {
-      // @ts-ignore
-      ok: (response) => resolve(response),
-      // @ts-ignore
-      fail: (error) => reject(error),
+    // This gets the JSESSIONID cookie to be set on the jsdom window
+    await new Promise((resolve, reject) => {
+      window.ScramShaClient.authenticate(`${siteUrlFormatted}/`, username, password, window.sjcl.hash.sha256, {
+        // @ts-ignore
+        ok: (response) => resolve(response),
+        // @ts-ignore
+        fail: (error) => reject(error),
+      });
     });
-  });
 
-  window.close();
+    // This activates the JSESSIONID cookie on Niagara's system
+    const loginCookies = dom.cookieJar.getCookiesSync(siteUrlFormatted);
+    await fetch(`${siteUrlFormatted}/j_security_check/`, {
+      method: 'GET',
+      headers: {
+        Cookie: loginCookies.map((cookie) => `${cookie.key}=${cookie.value}`).join('; '),
+      },
+    });
 
-  // This activates the JSESSIONID cookie on Niagara's system
-  const loginCookies = dom.cookieJar.getCookiesSync(siteUrl);
-  await fetch(`${siteUrl}/j_security_check/`, {
-    method: 'GET',
-    headers: {
-      Cookie: loginCookies.map((cookie) => `${cookie.key}=${cookie.value}`).join('; '),
-    },
-  });
-
-  return loginCookies;
+    return loginCookies;
+  } finally {
+    window.close();
+  }
 }
 
 // export async function logout(params) {}
